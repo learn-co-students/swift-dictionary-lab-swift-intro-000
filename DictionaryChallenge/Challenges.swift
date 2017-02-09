49a1791ac6ae565ea464bbf8e394b6b0c15dbbb9
larenSakota <jedi.temple@yahoo.com>
2017-02-08 21:09:06 -0800

    Nearly there

--- DictionaryChallenge/Challenges.swift
+++ DictionaryChallenge/Challenges.swift
@@ -13,8 +13,58 @@ class Challenges {
     
     
     
-    // Answer the problems here.
+    func createStatesAndCapitals() -> [String : String] {
+       let results = [
+        "New York" : "Albany",
+        "Ohio" : "Columbus",
+        "Florida" : "Tallahassee",
+        "Georgia" : "Atlanta",
+        "Kentucky" : "Frankfort"
+        ]
+        
+        return results
+    }
     
+    func floridaCapital() -> String? {
+        let florida = createStatesAndCapitals()
+        return florida["Florida"]
+    }
     
+    func createFloridaCapitalSentence() -> String {
+        if let disneyWorld = floridaCapital() {
+            return "The capital of Florida is \(disneyWorld)."
+        } else {
+            return "Unable to find the capital of Florida."
+        }
+    }
+    
+    func pennsylvaniaCapital() -> String? {
+        let libertyBell = createStatesAndCapitals()
+        return libertyBell["Pennsylvania"]
+    }
+    
+    func createPennsylvaniaSentence() -> String {
+        if let penn = pennsylvaniaCapital() {
+            return "The capital of Pennsylvania is \(penn)."
+        } else {
+            return "Unable to find the captial of Pennsylvania."
+        }
+    }
+    
+    func createAllStatesAndCapitals() -> [String : String] {
+        var allStates = createStatesAndCapitals()
+        allStates["Pennsylvania"] = "Harrisburg"
+        return allStates
+    }
+    
+    func removePennsylvania() -> [String : String] {
+        var allStates = createStatesAndCapitals()
+        allStates["Pennsylvania"] = nil
+        return allStates
+    }
+    
+    func createBand() -> [String : [String]] {
+        
+    }
     
 }
