d7910198a6a7cca12b34e32191a76856d8a5f283
g3kenny <g3_kenny@hotmail.com>
2017-02-26 17:32:02 +0800

    Commit dictionary lab1

--- DictionaryChallenge/Challenges.swift
+++ DictionaryChallenge/Challenges.swift
@@ -14,7 +14,57 @@ class Challenges {
     
     
     // Answer the problems here.
+    func createStatesAndCapitals() -> [String:String] {
+        return ["New York":"Albany",
+                "Ohio":"Columbus",
+                "Florida":"Tallahassee",
+                "Georgia":"Atlanta",
+                "Kentucky":"Frankfort"]
+    }
     
+    func floridaCapital() -> String? {
+        let dictionary = createStatesAndCapitals()
+        return dictionary["Florida"]
+    }
     
+    func createFloridaCapitalSentence() -> String {
+        let florida = floridaCapital()
+        if let florida = florida {
+            return"The capital of Florida is \(florida)."
+        }
+        
+        return "Unable to find the capital of Florida."
+    }
     
+    func pennsylvaniaCapital() -> String? {
+        let dictionary = createStatesAndCapitals()
+        return dictionary["Pennsylvania"]
+    }
+    
+    func createPennsylvaniaSentence() -> String {
+        let pennsylvania = pennsylvaniaCapital()
+        if let pennsylvania = pennsylvania {
+            return"The capital of Pennsylvania is \(pennsylvania)."
+        }
+        
+        return "Unable to find the capital of Pennsylvania."
+    }
+    
+    func createAllStatesAndCapitals() -> [String:String] {
+        var dictionary = createStatesAndCapitals()
+        dictionary["Pennsylvania"] = "Harrisburg"
+        return dictionary
+    }
+    
+    func removePennsylvania() -> [String:String] {
+        var dictionary = createAllStatesAndCapitals()
+        dictionary["Pennsylvania"] = nil
+        return dictionary
+    }
+    
+    func createBand() -> [String:[String]] {
+        return ["Nirvana":["Kurt Cobain", "Krist Novoselic", "Dave Grohl"],
+                "The Beatles":["John Lennon", "George Harrison", "Paul McCartney", "Ringo Starr"],
+                "The Breeders":["Kim Deal", "Kelley Deal", "Josephine Wiggs", "Jim Macpherson"]]
+    }
 }
