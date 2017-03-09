25bcad88adc47127223c95315e3333b992b8078a
Ryan Lee <rlee33@gmail.com>
2017-03-09 15:57:40 -0600

    implemented functions for lab questions 1-5

--- DictionaryChallenge/Challenges.swift
+++ DictionaryChallenge/Challenges.swift
@@ -11,10 +11,46 @@ import Foundation
 
 class Challenges {
     
+    func createStatesAndCapitals() -> [String: String] {
+        var statesAndCapitals: [String: String] = [:]
+        
+        statesAndCapitals.updateValue("Albany", forKey: "New York")
+        statesAndCapitals.updateValue("Columbus", forKey: "Ohio")
+        statesAndCapitals.updateValue("Tallahassee", forKey: "Florida")
+        statesAndCapitals.updateValue("Atlanta", forKey: "Georgia")
+        statesAndCapitals.updateValue("Frankfort", forKey: "Kentucky")
+        
+        return statesAndCapitals
+    }
     
+    func floridaCaptial() -> String? {
+        
+        let statesAndCapitals = createStatesAndCapitals()
+        
+        return statesAndCapitals["Florida"]
+    }
     
-    // Answer the problems here.
+    func createFloridaCapitalSentence() -> String {
+        if let floridaCapital = floridaCaptial() {
+            return "The capital of Florida is \(floridaCapital)."
+        }
+        return "unable to find the capital of Florida."
+    }
     
+    func pennsylvaniaCapital() -> String? {
+        let statesAndCapitals = createStatesAndCapitals()
+        let pennsylvaniaCapital = statesAndCapitals["Pennsylvania"]
+        return pennsylvaniaCapital
+    }
     
+    func createpennsylvaniaSentence() -> String {
+        if let pennsylvaniaCapital = pennsylvaniaCapital() {
+            return "The capital of Pennsylvania is \(pennsylvaniaCapital)."
+        } else {
+            return "Unable to find the capital of Pennsylvania."
+        }
+    }
+    
+
     
 }
