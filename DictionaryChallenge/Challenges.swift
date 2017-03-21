ce064c989abe7d72161e9fc13d4997e3ad274ebc
RaymondAtivie <raymondativie@gmail.com>
2017-03-21 16:51:33 +0100

    Ans

--- DictionaryChallenge/Challenges.swift
+++ DictionaryChallenge/Challenges.swift
@@ -14,7 +14,88 @@ class Challenges {
     
     
     // Answer the problems here.
+    func createStatesAndCapitals() -> [String: String]{
+        let SC = [
+            "New York":"Albany",
+            "Ohio":"Columbus",
+            "Florida":"Tallahassee",
+            "Georgia":"Atlanta",
+            "Kentucky":"Frankfort",
+        ]
+        
+        return SC
+    }
     
+    func floridaCapital() -> String? {
+        let cap = createStatesAndCapitals()
+        
+        return cap["Florida"]
+    }
     
+    func createFloridaCapitalSentence() -> String{
+        if let city = floridaCapital(){
+            return "The capital of Florida is \(city)."
+        }else{
+            return "Unable to find the capital of Florida."
+        }
+    }
     
+    func pennsylvaniaCapital() -> String?{
+        let nd = createStatesAndCapitals()
+        return nd["Pennsylvania"]
+    }
+    
+    func createPennsylvaniaSentence() -> String{
+        if let city = pennsylvaniaCapital(){
+            return "The capital of Pennsylvania is \(city)."
+        }else{
+            return "Unable to find the capital of Pennsylvania."
+        }
+    }
+    
+    func createAllStatesAndCapitals() -> [String: String]{
+        var newList = createStatesAndCapitals()
+        newList["Pennsylvania"] = "Harrisburg"
+        
+        return newList
+    }
+    
+    func removePennsylvania() -> [String: String]{
+        var nL = createAllStatesAndCapitals()
+        
+        nL["Pennsylvania"] = nil
+        
+        return nL
+    }
+    
+    func createBand() -> [String: [String]]{
+        let Nirvana = ["Kurt Cobain", "Krist Novoselic", "Dave Grohl"]
+        let Beatles = ["John Lennon", "George Harrison", "Paul McCartney", "Ringo Starr"]
+        let Breeders = ["Kim Deal", "Kelley Deal", "Josephine Wiggs", "Jim Macpherson"]
+        
+        let bands = [
+            "Nirvana": Nirvana,
+            "The Beatles": Beatles,
+            "The Breeders": Breeders
+        ]
+        
+        return bands
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
