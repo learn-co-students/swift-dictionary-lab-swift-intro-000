8fac1c43d380f570b51af79538a72e8aa04a3b8b
Zainab-Kalief <alesewura@yahoo.com>
2017-03-22 12:14:40 +0100

    done

--- DictionaryChallenge/Challenges.swift
+++ DictionaryChallenge/Challenges.swift
@@ -16,5 +16,117 @@ class Challenges {
     // Answer the problems here.
     
     
+    //1
+    
+    func createStatesAndCapitals() -> [String: String] {
+    
+        let statesAndCapitals = [
+            "New York":  "Albany",
+            "Ohio": "Columbus",
+            "Florida": "Tallahassee",
+            "Georgia": "Atlanta",
+            "Kentucky": "Frankfort"
+        ]
+  
+                
+        return statesAndCapitals
+    }
+    
+    //2
+    
+    func floridaCapital() -> String? {
+        
+        let statesAndCapital = createStatesAndCapitals()
+        return statesAndCapital["Florida"]
+    }
+    
+    //3.
+    
+    func createFloridaCapitalSentence() -> String {
+        
+        if let floridaCapital =  floridaCapital() {
+            return "The capital of Florida is \(floridaCapital)."
+        } else {
+            return "Unable to find the capital of Florida."
+        }
+     
+    }
+    
+    //4.
+    
+    func pennsylvaniaCapital() -> String? {
+        
+        let statesAndCapital = createStatesAndCapitals()
+        
+        return statesAndCapital["Pennsylvania"]
+    }
+    
+   //5.
+    
+    func createPennsylvaniaSentence() -> String {
+        
+        if let pennsylvaniaCapital = pennsylvaniaCapital() {
+            return "The capital of Pennsylvania is \(pennsylvaniaCapital)."
+        } else {
+            return "Unable to find the capital of Pennsylvania."
+        }
+    }
+    
+    //6.
+    
+    func createAllStatesAndCapitals() -> [String: String] {
+        
+        var allStatesAndCapitals = createStatesAndCapitals()
+        
+        allStatesAndCapitals.updateValue("Harrisburg", forKey: "Pennsylvania")
+        
+        return allStatesAndCapitals
+        
+        
+    }
+    
+    //7.
+    
+    func removePennsylvania() -> [String: String] {
+        
+        var allStatesAndCapitals = createAllStatesAndCapitals()
+        
+        allStatesAndCapitals.removeValue(forKey: "Pennsylvania")
+        
+        return allStatesAndCapitals
+    }
+    
+    //8.
+    
+    func createBand() -> [String: [String]] {
+        
+        let nirvanaBand = ["Kurt Cobain",
+                           "Krist Novoselic",
+                           "Dave Grohl"]
+        
+        let theBeatlesBand = ["John Lennon",
+                              "George Harrison",
+                              "Paul McCartney",
+                              "Ringo Starr"]
+        
+        let theBreedersBand = ["Kim Deal",
+                               "Kelley Deal",
+                               "Josephine Wiggs",
+                               "Jim Macpherson"]
+        
+        
+        let band = ["Nirvana": nirvanaBand,
+                    "The Beatles": theBeatlesBand,
+                    "The Breeders": theBreedersBand]
+        
+  
+        return band
+    }
+    
+    
+    
+    
+    
+    
     
 }
