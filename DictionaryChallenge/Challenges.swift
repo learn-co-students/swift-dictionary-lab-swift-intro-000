f24ecfb341461b8a88d52fcbd77d061613edca12
Mitchell Schuster <mgschuster11@gmail.com>
2017-07-17 16:44:21 +0300

    Finished

--- DictionaryChallenge/Challenges.swift
+++ DictionaryChallenge/Challenges.swift
@@ -11,9 +11,83 @@ import Foundation
 
 class Challenges {
     
+    func createStatesAndCapitals() -> [String: String] {
+        let statesAndCapitals = [
+            "New York": "Albany",
+            "Ohio": "Columbus",
+            "Florida": "Tallahassee",
+            "Georgia": "Atlanta",
+            "Kentucky": "Frankfort",
+        ]
+        
+        return statesAndCapitals
+    }
+    
+    func floridaCapital() -> String? {
+        var florida = createStatesAndCapitals()
+        return florida["Florida"]
+    }
+    
+    func createFloridaCapitalSentence() -> String {
+        if let capital = floridaCapital() {
+            return "The capital of Florida is \(capital)."
+        } else {
+            return "Unable to find the capital of Florida."
+        }
+    }
+    
+    func pennsylvaniaCapital() -> String? {
+        var pennsylvania = createStatesAndCapitals()
+        return pennsylvania["Pennsylvania"]
+    }
+    
+    func createPennsylvaniaSentence() -> String {
+        if let capital = pennsylvaniaCapital() {
+            return "The capital of Pennsylvania is \(capital)."
+        } else {
+            return "Unable to find the capital of Pennsylvania."
+        }
+    }
+    
+    func createAllStatesAndCapitals() -> [String:String] {
+        var final = createStatesAndCapitals()
+        
+        final["Pennsylvania"] = "Harrisburg"
+        
+        return final
+    }
+    
+    func removePennsylvania() -> [String:String] {
+        var remove = createAllStatesAndCapitals()
+        
+        remove["Pennsylvania"] = nil
+        
+        return remove
+    }
+    
+    func createBand() -> [String:[String]] {
+        
+        let band = [
+            "Nirvana" : ["Kurt Cobain", "Krist Novoselic", "Dave Grohl"],
+            "The Beatles" : ["John Lennon", "George Harrison", "Paul McCartney", "Ringo Starr"],
+            "The Breeders" : ["Kim Deal", "Kelley Deal", "Josephine Wiggs", "Jim Macpherson"],
+        ]
+        
+        return band
+        
+    }
     
     
-    // Answer the problems here.
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     
