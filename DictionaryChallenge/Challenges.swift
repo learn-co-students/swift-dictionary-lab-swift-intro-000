d98051ca430fde2bed4ea5d6adf88792630ae9f7
Trevonte <trevontedixon@gmail.com>
2017-11-29 04:08:12 -0500

    dictionary lab w solutions

--- DictionaryChallenge/Challenges.swift
+++ DictionaryChallenge/Challenges.swift
@@ -14,7 +14,58 @@ class Challenges {
     
     
     // Answer the problems here.
+    func createStatesAndCapitals() -> [String:String] {
+        let statesWithCapitals = ["New York":"Albany","Ohio":"Columbus","Florida":"Tallahassee","Georgia":"Atlanta","Kentucky":"Frankfort"]
+
+        return statesWithCapitals
+    }
     
     
+    func floridaCapital() -> String? {
+        let newStatesWithCapitals = createStatesAndCapitals()
+        return newStatesWithCapitals["Florida"]
+    }
     
+    func createFloridaCapitalSentence() -> String {
+        if let floridaCapital = floridaCapital() {
+            return "The capital of Florida is \(floridaCapital)."
+        } else {
+            return "Unable to find the capital of Florida."
+        }
+    }
+    
+    func pennsylvaniaCapital() -> String? {
+        let newerStatesWithCapitals = createStatesAndCapitals()
+        return newerStatesWithCapitals["Pennsylvania"]
+    }
+    
+    func createPennsylvaniaSentence() -> String {
+        if let pennsylvaniaCapitol = pennsylvaniaCapital() {
+            return "The capitol of Pennsylvania is \(pennsylvaniaCapitol)."
+        } else {
+            return "Unable to find the capital of Pennsylvania."
+        }
+    }
+    
+    func createAllStatesAndCapitals() -> [String:String] {
+        var newestStatesAndCapitols = createStatesAndCapitals()
+        newestStatesAndCapitols["Pennsylvania"] = "Harrisburg"
+        return newestStatesAndCapitols
+    }
+    
+    func removePennsylvania() -> [String:String] {
+        var myplaces = createAllStatesAndCapitals()
+        myplaces["Pennsylvania"] = nil
+        return myplaces
+    }
+    
+    func createBand() -> [String:[String]] {
+    let nirvannaMembers = ["Kurt Cobain", "Kris Novoselic","Dave Grohl"]
+    let beatlesMembers = ["John Lennon", "George Harrison", "Paul McCartney", "Ringo Star"]
+    let breedersMembers = ["Kim Deal","Kelley Deal","Josephine Wiggs","Jim Macpherson"]
+    
+        var favoriteBands = ["Nirvana": nirvannaMembers,"The Beatles":beatlesMembers, "The Breeders":breedersMembers]
+        
+        return favoriteBands
+    }
 }
