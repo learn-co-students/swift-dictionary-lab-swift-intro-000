339d1c46646107fd1d4ff33c12e45ad6b0c9b8b5
ericjlein <35666587+ericjlein@users.noreply.github.com>
2018-03-06 22:45:04 -0500

    Add files via upload

--- DictionaryChallenge/Challenges.swift
+++ DictionaryChallenge/Challenges.swift
@@ -13,8 +13,68 @@ class Challenges {
     
     
     
-    // Answer the problems here.
+    func createStatesAndCapitals() -> [String: String] {
+        var statesAndCapitals = [
+            "New York": "Albany",
+            "Ohio": "Columbus",
+            "Florida": "Tallahassee",
+            "Georgia": "Atlanta",
+            "Kentucky": "Frankfort"
+        ]
+        return statesAndCapitals
+    }
     
+    func floridaCapital() -> String? {
+        let florida = createStatesAndCapitals()
+        return florida["Florida"]
+    }
+    
+    func createFloridaCapitalSentence() -> String {
+        if let floridaCapital = floridaCapital() {
+            return "The capital of Florida is \(floridaCapital)."
+        }
+        else {
+            return "Unable to find capital of Florida"
+        }
+    }
+    
+    func pennsylvaniaCapital() -> String? {
+        let pennsylvania = createStatesAndCapitals()
+        return pennsylvania["Pennsylvania"]
+    }
+    
+    func createPennsylvaniaSentence() -> String {
+        if let pennsylvaniaCapital = pennsylvaniaCapital() {
+            return "The capital of Pennsylvania is \(pennsylvaniaCapital)"
+        }
+        else {
+            return "Unable to find the capital of Pennsylvania."
+        }
+    }
+    
+    func createAllStatesAndCapitals() -> [String: String] {
+        var allStatesAndCapitals = createStatesAndCapitals()
+        allStatesAndCapitals["Pennsylvania"] = "Harrisburg"
+        return allStatesAndCapitals
+    }
+    
+    func removePennsylvania() -> [String: String] {
+        var remPenn = createAllStatesAndCapitals()
+        remPenn["Pennsylvania"] = nil
+        return remPenn
+    }
+    
+    func createBand() -> [String: [String]] {
+        let nirvana = ["Kurt Cobain", "Krist Novoselic", "Dave Grohl"]
+        let theBeatles = ["John Lennon", "George Harrison", "Paul McCartney", "Ringo Starr"]
+        let theBreeders = ["Kim Deal", "Kelley Deal", "Josephine Wiggs", "Jim Macpherson"]
+        var favoriteBands = [
+            "Nirvana": nirvana,
+            "The Beatles": theBeatles,
+            "The Breeders": theBreeders
+        ]
+        return favoriteBands
+    }
     
     
 }
