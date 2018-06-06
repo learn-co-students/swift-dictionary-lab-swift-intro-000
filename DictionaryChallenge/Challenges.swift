eeebd761b3ba287a03e391e5397dce28db0376e7
Jayymash <mashnijason@gmail.com>
2018-06-06 14:54:46 -0400

    Dictionaries are not fun

--- DictionaryChallenge/Challenges.swift
+++ DictionaryChallenge/Challenges.swift
@@ -11,9 +11,93 @@ import Foundation
 
 class Challenges {
     
+    func createStatesAndCapitals() -> [String : String] {
+        return [
+            "New York":  "Albany",
+            "Ohio": "Columbus",
+            "Florida": "Tallahassee",
+            "Georgia": "Atlanta",
+            "Kentucky": "Frankfort",
+        ]
+    }
+    
+    func floridaCapital() -> String? {
+       
+        let getCapitalOf = createStatesAndCapitals()
+        
+        return getCapitalOf["Florida"]
+    }
+
+    func createFloridaCapitalSentence() -> String {
+        if let capital = floridaCapital() {
+            return ("The capital of Florida is \(capital).")
+        }else{
+            return ("Unable to find the captial of Florida")
+        }
+    }
+    
+    func pennsylvaniaCapital() -> String? {
+        let pennyCapital = createStatesAndCapitals()
+        return pennyCapital["pennsylvania"]
+    }
+    
+    func createPennsylvaniaSentence() -> String {
+        
+        if let capital = pennsylvaniaCapital() {
+            return "The capitol of Pennsylvania is \(capital)."
+        }else{
+            return ("Unable to find the capital of Pennsylvania.")
+        }
+    }
+    
+    func createAllStatesAndCapitals() -> [String: String] {
+        
+        var pennsylvaniaCapital = createStatesAndCapitals()
+        pennsylvaniaCapital["Pennsylvania"] = "Harrisburg"
+        return pennsylvaniaCapital
+    }
+    
+    func removePennsylvania() -> [String: String] {
+        
+        var removePennsylvania = createStatesAndCapitals()
+        removePennsylvania.removeValue(forKey: "Pennsylvania")
+        return removePennsylvania
+    }
+    
+    func createBand() -> [String: [String]] {
+        
+        let nirvanaMembers = ["Kurt Cobain", "Krist Novoselic", "Dave Grohl"]
+        
+        let theBeatlesMembers = ["John Lennon:", "George Harrison", "Paul McCartney", "Ringo Starr"]
+        
+        let theBreedersMembers = ["Kim Deal", "Kelley Deal", "Josephine Wiggs", "Jim Macpherson"]
+        
+        let fullBandGathering = [
+            "Nirvana": nirvanaMembers,
+            "The Breeders": theBreedersMembers,
+            "The Beatles": theBeatlesMembers
+                                 ]
+        
+        return fullBandGathering
+    }
     
     
-    // Answer the problems here.
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     
