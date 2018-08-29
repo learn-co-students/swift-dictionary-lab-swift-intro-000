a4fd712a898dca0536043b78993aa54bcc79b203
mkes99 <mike@formativewebsolutions.com>
2018-08-28 22:05:41 -0700

    Dictionaries

--- DictionaryChallenge/Challenges.swift
+++ DictionaryChallenge/Challenges.swift
@@ -15,6 +15,70 @@ class Challenges {
     
     // Answer the problems here.
     
+    func createStatesAndCapitals() -> [String : String]{
+        return [
+            "New York" : "Albany",
+            "Ohio" : "Columbus",
+            "Florida" : "Tallahassee",
+            "Georgia" : "Atlanta",
+            "Kentucky" : "Frankfort"
+        ]
+    }
+    
+    func floridaCapital() -> String?{
+        return createStatesAndCapitals()["Florida"]
+    }
+    
+    func createFloridaCapitalSentence() -> String{
+        guard let capitalOfFlorida = floridaCapital() else {
+            return "Unable to find the capital of Florida."
+        }
+        return "The capital of Florida is \(capitalOfFlorida)."
+    }
+    
+    // Technically I dont need the floridaCapital function I could have just handled this all in the createFloridaCapitalSentence function by calling createStateAndCapital()["Florida"] instead of floridaCapital() correct?
+    
+    func pennsylvaniaCapital() -> String? {
+        return createStatesAndCapitals()["Pennsylvania"]
+    }
+    
+    func createPennsylvaniaSentence() -> String{
+        guard let capitalofPennsylvania = pennsylvaniaCapital() else {
+            return "Unable to find the capital of Pennsylvania."
+        }
+        return "The capital of Florida is \(capitalofPennsylvania)."
+    }
+    
+    func createAllStatesAndCapitals() -> [String : String] {
+        var updateDictionary = createStatesAndCapitals()
+        updateDictionary["Pennsylvania"] = "Harrisburg"
+        
+        return updateDictionary
+    }
+    
+    func removePennsylvania() -> [String: String]{
+        var updateDictionary = createAllStatesAndCapitals()
+        updateDictionary["Pennsylvania"] = nil
+        
+        return updateDictionary
+    }
+
+    
+    func createBand() -> [String : [String]] {
+        let nirvanaMemebers = ["Kurt Cobain", "Krist Novoselic", "Dave Grohl"]
+        let beatlesMembers = ["John Lennon", "George Harrison", "Paul McCartney", "Ringo Starr"]
+        let breedersMembers = ["Kim Deal", "Kelley Deal", "Josephine Wiggs", "Jim Macpherson"]
+        
+        return [
+            "Nirvana": nirvanaMemebers,
+            "The Beatles": beatlesMembers,
+            "The Breeders": breedersMembers
+        ]
+        
+        // I can see the usefulness of being able to do this, but in this function it doesnt make sences I should have just built the arrays in the dictionary correct?
+    }
+    
+    
     
     
 }
